bcad60b55061f47c6fb8a1470ccb3c7abe7c59e7
Adriano Gonçalves <adgoleva@gmail.com>
2022-04-21 09:27:02 -0300

    List users

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,58 +1,44 @@
 import type { NextPage } from 'next';
-import { AnnotationIcon, GlobeAltIcon, LightningBoltIcon, ScaleIcon } from '@heroicons/react/outline';
+import {useEffect, useState} from "react";
+
+interface UserType {
+    id: number;
+    name: string;
+    email: string;
+}
 
 const Home: NextPage = () => {
-    const features = [
-        {
-            name: 'Competitive exchange rates',
-            description:
-                'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-            icon: GlobeAltIcon,
-        },
-        {
-            name: 'No hidden fees',
-            description:
-                'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-            icon: ScaleIcon,
-        },
-        {
-            name: 'Transfers are instant',
-            description:
-                'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-            icon: LightningBoltIcon,
-        },
-        {
-            name: 'Mobile notifications',
-            description:
-                'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-            icon: AnnotationIcon,
-        },
-    ]
+    const [data, setData] = useState<UserType[]>();
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((res) => res.json())
+            .then((data) => {
+                setData(data)
+            })
+    },[]);
+
+    if (!data) {
+        return <div>Loading...</div>
+    }
+
   return (
       <div className="py-12 bg-white">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
               <div className="lg:text-center">
-                  <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">Transactions</h2>
-                  <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                      A better way to send money
-                  </p>
-                  <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-                      Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in
-                      accusamus quisquam.
-                  </p>
+                  <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">Users</h2>
               </div>
 
               <div className="mt-10">
                   <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
-                      {features.map((feature) => (
-                          <div key={feature.name} className="relative">
+                      {data.map((item: UserType) => (
+                          <div key={item.id} className="relative">
                               <dt>
-                                  <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                                      <feature.icon className="h-6 w-6" aria-hidden="true" />
-                                  </div>
-                                  <p className="ml-16 text-lg leading-6 font-medium text-gray-900">{feature.name}</p>
+                                  <p className="ml-16 text-lg leading-6 font-medium text-gray-900">{item.name}</p>
                               </dt>
-                              <dd className="mt-2 ml-16 text-base text-gray-500">{feature.description}</dd>
+                              <dd className="mt-2 ml-16 text-base text-gray-500">
+                                  <strong>@</strong> {item.email}
+                              </dd>
                           </div>
                       ))}
                   </dl>
